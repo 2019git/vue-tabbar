c9e43cc23d5ef0c471a37959e9957281a56887da
wangzhijun <wangzhijun@DESKTOP-H0VSEF1>
2021-04-22 16:33:38 +0800

    修改图标

--- src/components/TabBarMain.vue
+++ src/components/TabBarMain.vue
@@ -1,23 +1,23 @@
 <template>
   <TabBar>
     <TabBarItem path = "/home">
-      <img slot="item-icon" src="@/assets/img/tabbar/home.png">
-      <img slot="item-icon-active" src="@/assets/img/tabbar/home-active.png">
+      <img slot="item-icon" src="@/assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/home_active.svg">
       <div slot="item-name">首页</div>
     </TabBarItem>
-    <TabBarItem path = "/category" activeColor="blue">
-      <img slot="item-icon" src="@/assets/img/tabbar/category.png">
-      <img slot="item-icon-active" src="@/assets/img/tabbar/category-active.png">
+    <TabBarItem path = "/category" activeColor="red">
+      <img slot="item-icon" src="@/assets/img/tabbar/category.svg">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/category_active.svg">
       <div slot="item-name">分类</div>
     </TabBarItem>
-    <TabBarItem path = "/shopcart" activeColor="pink">
-      <img slot="item-icon" src="@/assets/img/tabbar/shopcart.png">
-      <img slot="item-icon-active" src="@/assets/img/tabbar/shopcart-active.png">
+    <TabBarItem path = "/shopcart" activeColor="red">
+      <img slot="item-icon" src="@/assets/img/tabbar/shopcart.svg">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/shopcart_active.svg">
       <div slot="item-name">购物车</div>
     </TabBarItem>
     <TabBarItem path = "/profile">
-      <img slot="item-icon" src="@/assets/img/tabbar/profile.png">
-      <img slot="item-icon-active" src="@/assets/img/tabbar/profile-active.png">
+      <img slot="item-icon" src="@/assets/img/tabbar/profile.svg">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/profile_active.svg">
       <div slot="item-name">我的</div>
     </TabBarItem>
   </TabBar>
